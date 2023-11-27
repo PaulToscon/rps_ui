b87a4829ae61408596dd18e0238deb384182696c
PaulToscon <134002@mail.unacar.mx>
2023-11-27 03:57:09 -0600

    second commit of the RPS with the UI

--- main.js
+++ main.js
@@ -1,9 +1,114 @@
-let counter = 0;
-while(counter < 5){
-    counter++;
-    console.log(`Match number ${counter}`);
+window.onload = function(){
 
-let pChoice = prompt("Enter your choice: ");
+let pChoice = "";
+let playerChoice = "";
+let compChoice = "";
+let compScore = 0;
+let playerScore = 0;
+let gameOver = 5;
+
+// UI ELEMENTS
+
+const btnSelection = document.querySelectorAll('button');
+const displayChosenOptionPlayer = document.getElementById('selection-player');
+const displayChosenOptionComputer = document.getElementById('selection-computer');
+
+function updateChosenOption(player, computer, result){
+    //PLAYER DISPLAY
+    if(displayChosenOptionPlayer){
+    const playerOptionImage = displayChosenOptionPlayer.querySelector('#player-chosen');
+
+    playerOptionImage.src = `img/${player}.png`;
+    playerOptionImage.alt = player;
+
+    if (result.includes('won')) {
+        displayChosenOptionPlayer.style.backgroundColor = 'green';
+      } else if (result === "It's a tie!") {
+        displayChosenOptionPlayer.style.backgroundColor = 'yellow';
+      } else if (result.includes('lost')) {
+        displayChosenOptionPlayer.style.backgroundColor = 'red';
+      } else {
+        displayChosenOptionPlayer.style.backgroundColor = 'white';
+      }
+    }else{
+        console.log("Player option image not found");
+
+    }
+
+    //COMPUTER DISPLAY
+    if(displayChosenOptionComputer){
+    const computerOptionImage = displayChosenOptionComputer.querySelector('#computer-chosen');
+
+    computerOptionImage.src = `img/${computer}.png`;
+    computerOptionImage.alt = computer;
+
+    if (result.includes('won')) {
+        displayChosenOptionComputer.style.backgroundColor = 'red';
+      } else if (result === "It's a tie!") {
+        displayChosenOptionComputer.style.backgroundColor = 'yellow';
+      } else if (result.includes('lost')) {
+        displayChosenOptionComputer.style.backgroundColor = 'green';
+      } else {
+        displayChosenOptionComputer.style.backgroundColor = 'white';
+      }
+    }else{
+        console.log("Computer option image not found");
+    }
+}
+
+// Score display
+
+function updateCounters(){
+    const playerCounter = document.getElementById('pcounter');
+    const computerCounter = document.getElementById('ccounter');
+    
+    playerCounter.textContent = playerScore.toString();
+    computerCounter.textContent = compScore.toString();
+
+    if (playerScore >= gameOver || compScore >= gameOver) {
+        setTimeout(() => {
+        alert('Game over! Wanna play again?');
+        restartGame();
+        }, 200)
+      }
+
+}
+
+
+
+btnSelection.forEach((button) => {
+  button.addEventListener('click', () => {
+    pChoice = button.id;
+
+    playerChoice = pChoice;
+
+    compChoice = getComputerChoice();
+
+    console.log('Player choice:', playerChoice);
+    console.log('Computer choice:', compChoice);
+
+
+    const result = gameSession(compChoice, playerChoice);
+
+    console.log(result);
+    
+    updateChosenOption(pChoice, compChoice, result);
+    updateCounters();
+
+  });
+
+button.addEventListener('mouseover', () => {
+    button.style.backgroundColor = 'yellow';
+  });
+  button.addEventListener('mouseout', () => {
+    button.style.backgroundColor = '';
+  });
+
+});
+
+// LOGIC
+
+
 
 function getComputerChoice () {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -19,39 +124,34 @@ function getComputerChoice () {
     }
 }
 
-function getPlayerChoice (pChoice){
-    let playerChoice = pChoice.toLowerCase();
-    if(playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors"){
-        return playerChoice;
-    }else{
-        return pChoice;
-    }
-}
 
 function gameSession(getComputerChoice, getPlayerChoice){
-    let compChoice = getComputerChoice();
-    let playerChoice = getPlayerChoice(pChoice);
-    console.log(`Computer choice: ${compChoice}`);
-    console.log(`Player choice: ${playerChoice}`);
+    let computer = getComputerChoice;
+    let player = getPlayerChoice;
 
-    if(compChoice === playerChoice){
+    if(computer === player){
         return "It's a tie!";
-    }else if (compChoice === "rock" && playerChoice === "scissors"){
-        return `You lost! ${compChoice} beats ${playerChoice}`;
-    }else if (compChoice === "paper" && playerChoice === "rock"){
-        return `You lost! ${compChoice} beats ${playerChoice}`;
-    }else if (compChoice === "scissors" && playerChoice === "paper"){
-        return `You lost! ${compChoice} beats ${playerChoice}`;
-    } else if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
-        console.log(`Error! Player did not enter a valid choice! Match Aborted!`);
-        return counter--;
+    }else if (computer === "rock" && player === "scissors"){
+        compScore++;
+        return `You lost! ${computer} beats ${player}`; 
+    }else if (computer === "paper" && player === "rock"){
+        compScore++;
+        return `You lost! ${computer} beats ${player}`;
+    }else if (computer === "scissors" && player === "paper"){
+        compScore++;
+        return `You lost! ${computer} beats ${player}`;
     }else{
-        return `You won! ${playerChoice} beats ${compChoice}`;
+        playerScore++;
+        return `You won! ${player} beats ${computer}`;
     }
 }
 
-console.log(gameSession(getComputerChoice, getPlayerChoice));
-
+function restartGame(){
+    playerScore = 0;
+    compScore = 0;
+    updateCounters();
+    updateChosenOption('question', 'question', "")
 }
 
+}
 
